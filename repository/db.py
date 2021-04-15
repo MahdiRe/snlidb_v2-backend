b7222bdb9361aff3cd12f008ef6041a3f73ff442
Mahdi <mahdirefaideen@gmail.com>
2021-04-15 14:12:01 +0530

    API refine + DB err no. + user_repo

--- repository/db.py
+++ repository/db.py
@@ -23,13 +23,10 @@ class Db:
 
     def get_all_tables(self):
         try:
-            # Create cursor
             cursor = self._conn.cursor()
             cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_type = %s AND "
                            "table_schema=%s;", ('base table', 'test'))
-            # cursor.execute("SELECT * from student")
             self._conn.commit()
-            # tables = cursor.fetchall()
             tables = [dict((cursor.description[i][0], value)
                            for i, value in enumerate(row)) for row in cursor.fetchall()]
             cursor.close()
@@ -44,26 +41,24 @@ class Db:
             # Create cursor
             cursor = self._conn.cursor()
             cursor.execute("SELECT * FROM " + table + ";")
-            # cursor.execute("SELECT * from student")
             self._conn.commit()
-            # tables = cursor.fetchall()
             data = [dict((cursor.description[i][0], value)
                          for i, value in enumerate(row)) for row in cursor.fetchall()]
             cursor.close()
 
         except Exception as e:
             print('Exception : ' + str(e))
-            data = 'Exception found!'
+            if e.args[0] == 1146:
+                data = "Table '" + table + "' not found!"
+            else:
+                data = 'Exception found!'
         return data
 
     def get_columns(self, table):
         try:
-            # Create cursor
             cursor = self._conn.cursor()
-            cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME =   %s", table)
-            # cursor.execute(query)
+            cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = %s", table)
             self._conn.commit()
-            # records = cursor.fetchall()
             columns = [dict((cursor.description[i][0], value)
                             for i, value in enumerate(row)) for row in cursor.fetchall()]
             cursor.close()
@@ -71,3 +66,15 @@ class Db:
             print('Exception : ' + str(e))
             columns = 'Exception found!'
         return columns
+
+    def execute_query(self, query):
+        try:
+            cursor = self._conn.cursor()
+            cursor.execute(query)
+            self._conn.commit()
+            result = cursor.fetchall()
+            cursor.close()
+        except Exception as e:
+            print('Exception : ' + str(e))
+            result = 'Exception found!'
+        return result
--- repository/user_repo.py
+++ repository/user_repo.py
@@ -1,28 +1,32 @@
 from repository.db import Db
 
 
-class StudentRepo(Db):
+class UserRepo(Db):
 
     def __init__(self):
         super().__init__()
 
-    def insert_student(self, name, age, marks):
+    def insert_user(self, u_name, pwd, role):
         try:
             cursor = self._conn.cursor()
-            cursor.execute("INSERT INTO student(name, age, marks) VALUES (%s, %s, %s)", (name, age, marks))
+            cursor.execute("INSERT INTO users(user_name, password, role) values (%s, %s, %s)", (u_name, pwd, role))
             self._conn.commit()
             result = [dict((cursor.description[i][0], value)
                            for i, value in enumerate(row)) for row in cursor.fetchall()]
             cursor.close()
         except Exception as e:
             print('Exception : ' + str(e))
-            result = 'Exception found!'
+            if e.args[0] == 1062:
+                result = 'Username already taken!'
+            else:
+                result = 'Exception found!'
+        print(str(result))
         return result
 
-    def execute_student_query(self, query):
+    def login_user(self, u_name, pwd):
         try:
             cursor = self._conn.cursor()
-            cursor.execute(query)
+            cursor.execute("SELECT * FROM users WHERE user_name = %s and password = %s;", (u_name, pwd))
             self._conn.commit()
             result = [dict((cursor.description[i][0], value)
                            for i, value in enumerate(row)) for row in cursor.fetchall()]
@@ -30,4 +34,5 @@ class StudentRepo(Db):
         except Exception as e:
             print('Exception : ' + str(e))
             result = 'Exception found!'
+        print(result)
         return result
